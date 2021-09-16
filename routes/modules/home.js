2a67fde744ef9de6e336cade8ba190184996be6c
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-09-16 23:38:25 +0800

    Modify create Urldata function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,24 +8,20 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
-router.post('/', (req, res) => {
-  const originalUrl = req.body.originalUrl.trim()
-  UrlData.findOne({ originalUrl: originalUrl })
-  .lean()
-  .then(urlData => {
+router.post('/', async(req, res) => {
+  try {
+    const originalUrl = req.body.originalUrl.trim()
+    let urlData = await UrlData.findOne({ originalUrl: originalUrl }).lean()
+    
     if (urlData === null) {
       const shortUrl = generateUrl(originalUrl)
-      return UrlData.create({ originalUrl, shortUrl }).then(() => {
-          return UrlData.findOne({ originalUrl })
-          .lean()
-          .then(urlData => {
-            return res.render('index', { urlData })
-          })
-        })
-      }
-      return res.render('index', { urlData })
-    })
-    .catch(error => console.log(error))
+      await UrlData.create({ originalUrl, shortUrl })
+      urlData = await UrlData.findOne({ originalUrl }).lean()
+    }
+    res.render('index', { urlData })
+  } catch (error) {
+      console.log(error)
+  }
 })
 
 module.exports = router
